357541b3c75441c21305d76d01fc822fa140c7f1
kohilain16@gmail.com <kohilain16@gmail.com>
2023-11-23 15:58:59 +0000

    Edited wallet

--- client_code/wallet/__init__.py
+++ client_code/wallet/__init__.py
@@ -11,3 +11,7 @@ class wallet(walletTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass

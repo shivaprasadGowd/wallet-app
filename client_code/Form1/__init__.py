838f61e4f700e30388bc85135d98e6d154b52f4a
shivaprasadgowda9380@gmail.com <shivaprasadgowda9380@gmail.com>
2023-11-29 05:06:40 +0000

    Edited settings, Form1.ItemTemplate3 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,3 +11,7 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.repeating_panel_1.items = app_tables.customer.search(user=self.user)

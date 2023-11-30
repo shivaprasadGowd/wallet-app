ede99c9ede37865afa902392c0a7455b2a79bedc
thapa07lakshya@gmail.com <thapa07lakshya@gmail.com>
2023-11-30 10:21:16 +0000

    Edited LOGIN, ServerModule1, deposit, settings and 1 more

--- client_code/wallet/__init__.py
+++ client_code/wallet/__init__.py
@@ -14,7 +14,6 @@ class wallet(walletTemplate):
         self.user = user
         
         self.label_1.text = f"Welcome to Green Gate Financial, {user['username']}"
-        self.button_2.visible = False
         self.bank_details_visible = False
         self.label_bank_details_error = Label(text="", role="alert")
         self.label_bank_name.visible = False
@@ -32,28 +31,6 @@ class wallet(walletTemplate):
         self.text_box_2.visible=False
         self.drop_down_1.visible=False
        
-        
-
-    def button_1_click(self, **event_args):
-        # Get the user-entered Casa account number from the textbox
-        casa_number = self.textbox_account_number.text
-
-        # Validate if the entered Casa account number is valid (you can add more validation as needed)
-        if casa_number and casa_number.isdigit():
-            casa_number = int(casa_number)
-
-            # Check if the entered Casa account number is already in use
-            if not app_tables.accounts.search(q.equal('casa', casa_number)):
-                # Save the Casa account number to the user
-                app_tables.accounts.add_row(user=self.user, casa=casa_number)
-                self.label_2.text = f"Casa account {casa_number} created successfully."
-                self.button_2.visible = True
-                self.button_2.text = "Make Digital Wallet"
-            else:
-                self.label_2.text = "Casa account number already in use. Please choose a different one."
-        else:
-            self.label_2.text = "Please enter a valid Casa account number."
-
     def button_add_bank_details_click_click(self, **event_args):
        # Toggle the visibility of bank details labels and textboxes
         self.bank_details_visible = not self.bank_details_visible
@@ -82,7 +59,6 @@ class wallet(walletTemplate):
       account_Type = self.drop_down_1.selected_value
       wallet3 = anvil.server.call('generate_unique_id', self.user['username'], self.user['phone'])
       
-      
       if bank_name and account_number and ifsc_code and account_holder_name and branch_name and account_Type:
         # Save the bank details to the 'accounts' table
         
@@ -101,24 +77,10 @@ class wallet(walletTemplate):
       else:
         self.label_bank_details_error.text = "Please fill in all bank details."
 
+    def link_1_click(self, **event_args):
+      open_form('deposit')
 
-    def link_user_to_casa(self, user_id, casa_number):
-    # Call the server function to link the user to a Casa account
-      casa_row = anvil.server.call('link_user_to_casa', user_id=user_id, casa_number=casa_number)
 
-      if casa_row:
-        self.label_2.text = "User linked to Casa account successfully."
-
-    def map_casa_to_digital_wallet(self, user_id, casa_number):
-        # Call the server function to map the Casa account to a digital wallet
-        digital_row = anvil.server.call('map_casa_to_digital_wallet', user_id=user_id, casa_number=casa_number)
-
-        if digital_row:
-            self.label_2.text = "Casa account mapped to digital wallet successfully."
-            self.button_2.visible = True
-            self.button_2.text = "Make Digital Wallet"
-        else:
-            self.label_2.text = "Error mapping Casa account to digital wallet."
 
 
 

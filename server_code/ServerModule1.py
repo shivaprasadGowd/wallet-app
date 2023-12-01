9af8bd7f8c76607ecb97690d43c75b07b2a120d2
suryaacharya7892@gmail.com <suryaacharya7892@gmail.com>
2023-12-01 12:34:17 +0000

    Edited ServerModule1 and transfer

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -44,25 +44,34 @@ def generate_unique_id(username, phone):
     unique_id = f"{username}-{phone}"
 
     return unique_id
-
-@anvil.server.callable
-def depo(name,account,e_wallet,money):
-  current_datetime = datetime.now()
-  user_row = app_tables.transactions.add_row(
-    user=name,
-    account=account,
-    e_wallet=e_wallet,
-    money=money,
-    date=current_datetime
-  )
-  return user_row
-
-@anvil.server.callable
-def money(type,amount):
-  unique=f"{type}-{amount}"
-  return unique
   
+exchange_rates = {'usd': 73.5, 'eur': 87.2, 'inr': 1.0}
 
+@anvil.server.callable
+def get_exchange_rate(currency):
+    # Return the exchange rate for the specified currency
+    return exchange_rates.get(currency, 1.0)  # Default to 1.0 if currency not found
+
+@anvil.server.callable
+def extract_numeric_and_currency(amount_str):
+    # Initialize variables for numeric_part and currency_type
+    numeric_part = ''
+    currency_type = ''
+
+    # Iterate through characters in amount_str
+    for char in amount_str:
+        if char.isdigit() or char == '.':
+            numeric_part += char
+        elif char.isalpha():
+            currency_type += char
+
+    # Convert numeric_part to float if it contains a dot, otherwise to int
+    try:
+        numeric_part = float(numeric_part)
+    except ValueError:
+        numeric_part = 0  # Default to 0 if conversion fails
+
+    return numeric_part, currency_type
 
 @anvil.server.callable
 def transfer_money(user, amount, currency):
@@ -76,27 +85,43 @@ def transfer_money(user, amount, currency):
     user_row = app_tables.accounts.get(user=user)
     current_money_str = user_row['money']
 
-    # Convert the amount to rupees based on the selected currency
-    amount_in_rupees = convert_to_rupees(amount_numeric, exchange_rate)
+    # Ensure current_money_str is not None
+    current_money_str = current_money_str or '0'
 
-    # Update the 'e_money' column by adding the converted amount
-    user_row['e_money'] = str(float(user_row['e_money']) + amount_in_rupees)
+    # Convert the amount to rupees based on the selected currency
+    amount_in_rupees = amount_numeric * exchange_rate
+
+    # Ensure 'e_money' is not None
+    user_row['e_money'] = user_row['e_money'] or 0
+
+    # Update the 'e_money' column with the converted amount
+    user_row['e_money'] += amount_in_rupees
+
+    # Update the 'money' column by subtracting the transferred amount
+    user_row['money'] = str(float(current_money_str) - amount_numeric)
 
     # Save the changes to the row
     user_row.update()
 
-def extract_numeric_and_currency(amount_str):
-    # Extract numeric part and currency type from the string
-    numeric_part = ''.join(char for char in amount_str if char.isdigit() or char == '.')
-    currency_type = ''.join(char for char in amount_str if char.isalpha())
-    return float(numeric_part) if '.' in numeric_part else int(numeric_part), currency_type
+@anvil.server.callable
+def add_money(user, amount, currency):
+    # Extract numeric part and currency type from the 'amount' string
+    amount_numeric, source_currency = extract_numeric_and_currency(amount)
 
-def get_exchange_rate(currency):
-    # Implement logic to fetch exchange rate based on the selected currency
-    # For simplicity, you can have predefined rates or fetch them from an external API
-    exchange_rates = {'dollar': 73.5, 'euro': 87.2, 'rupees': 1.0}
-    return exchange_rates.get(currency, 1.0)
+    # Get the exchange rate for the selected currency
+    exchange_rate = get_exchange_rate(source_currency)
 
-def convert_to_rupees(amount, exchange_rate):
-    return amount * exchange_rate
+    # Fetch the current money value from the database
+    user_row = app_tables.accounts.get(user=user)
 
+    # Ensure 'e_money' is not None
+    user_row['e_money'] = user_row['e_money'] or 0
+
+    # Convert the amount to rupees based on the selected currency
+    amount_in_rupees = amount_numeric * exchange_rate
+
+    # Update the 'e_money' column with the converted amount
+    user_row['e_money'] += amount_in_rupees
+
+    # Save the changes to the row
+    user_row.update()

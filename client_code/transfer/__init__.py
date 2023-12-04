4595dc7b1c539cdd3f291f2b6b184307c7b45589
suryaacharya7892@gmail.com <suryaacharya7892@gmail.com>
2023-12-04 09:58:59 +0000

    Edited ServerModule1 and transfer

--- client_code/transfer/__init__.py
+++ client_code/transfer/__init__.py
@@ -9,22 +9,15 @@ from anvil.tables import app_tables
 class transfer(transferTemplate):
     def __init__(self, user=None, **properties):
         self.user = user
-        # Set Form properties and Data Bindings.
         self.init_components(**properties)
-
-    def button_1_click(self, **event_args):
-        # Get the amount and currency from the UI components
-        amount = float(self.text_box_4.text)
-        currency = self.drop_down_1.selected_value
-
-        # Extract the username from the user Row
-        username = self.user['username']
-
-        # Call the server function to transfer money
-        anvil.server.call('transfer_money', username, amount, currency)
-
-        # Optionally, update any UI elements or show a success message
-        self.label_4.text = f'Transferred {amount} {currency} successfully.'
+        self.display_balance() 
 
     def link_1_click(self, **event_args):
       open_form('customer', user= self)
+
+    def display_balance(self, user_casa):
+        user_row = app_tables.accounts.get(casa=user_casa)
+        self.label_6.text = f"USD Balance: {user_row['money_usd']}"
+        self.label_10.text = f"Euro Balance: {user_row['money_euro']}"
+        self.label_11.text = f"INR Balance: {user_row['money_inr']}"
+        self.label_12.text = f"Swiss Franc Balance: {user_row['money_swiss']}"

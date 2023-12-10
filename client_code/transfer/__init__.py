213988c94aed88db79af72c2671b1bcb1d2b5391
kohilain16@gmail.com <kohilain16@gmail.com>
2023-12-10 14:44:34 +0000

    Edited transfer

--- client_code/transfer/__init__.py
+++ client_code/transfer/__init__.py
@@ -117,12 +117,6 @@ class transfer(transferTemplate):
         if eb == '₹':
           self.label_14.text = (e_money_value)
 
-        
-
-        
-
-        
-
     def drop_down_2_change(self, **event_args):
       acc=self.dropdown_account_numbers.selected_value
       fore_money = anvil.server.call('get_accounts_emoney',acc)
@@ -144,54 +138,96 @@ class transfer(transferTemplate):
       self.display()
     
 
+    # def button_2_click(self, **event_args):
+    #   current_datetime = datetime.now()
+    #   acc = self.dropdown_account_numbers.selected_value
+    #   print(acc)
+    #   user_currency = anvil.server.call('get_currency_data', acc)
+    #   fore_money = anvil.server.call('get_accounts_emoney', acc)
+    #   selected_symbol = self.drop_down_3.selected_value
+    #   money_value = float(self.text_box_1.text)
+    #   user_for_emoney = self.user['username']
+    #   # conversion_rate_inr_to_usd = 0.012
+    #   # conversion_rate_swis_to_inr = 95.0
+    #   # conversion_rate_euro_to_inr = 90.0
+    #   if (money_value < 5) or (money_value > 50000):
+    #     self.label_18.text = "Money value should be between 5 and 50000 for a transfer Funds."
+    #     if selected_symbol == '$':
+    #         if float(fore_money['e_money']) > money_value:
+    #           fore_money['e_money'] = str(float(fore_money['e_money']) - (money_value * conversion_rate_inr_to_usd))
+    #           user_currency['money_usd'] = str(float(user_currency['money_usd']) + money_value)
+    #         else:
+    #           self.label_18.text = "no balance"
+    #     elif selected_symbol == '₹':
+    #         # Transfer e-money to INR (Not required as it's already in e-money)
+    #         if float(fore_money['e_money']) > money_value:
+    #             # Update e-money and INR balance
+    #             fore_money['e_money'] = str(float(fore_money['e_money']) - money_value)
+    #             user_currency['money_inr'] = str(float(user_currency['money_inr']) + money_value)
+
+    #             # Update currencies datatable for money_inr
+    #             app_tables.currencies.add_row(
+    #                 money_usd=0,
+    #                 money_inr=money_value,
+    #                 money_euro=0,  # Adjust these values based on conversions
+    #                 money_swis=0,
+    #                 date=current_datetime,
+    #                 transaction_type="no",
+    #                 user=self.user['username']
+    #             )
+    #         else:
+    #             self.label_18.text = "Insufficient e-money balance"
+
+
     def button_2_click(self, **event_args):
-      current_datetime = datetime.now()
-      acc = self.dropdown_account_numbers.selected_value
-      print(acc)
-      user_currency = anvil.server.call('get_currency_data', acc)
-      fore_money = anvil.server.call('get_accounts_emoney', acc)
-      selected_symbol = self.drop_down_3.selected_value
-      money_value = float(self.text_box_1.text)
-      user_for_emoney = self.user['username']
-      # conversion_rate_inr_to_usd = 0.012
-      # conversion_rate_swis_to_inr = 95.0
-      # conversion_rate_euro_to_inr = 90.0
-      if (money_value < 5) or (money_value > 50000):
-        self.label_18.text = "Money value should be between 5 and 50000 for a transfer Funds."
-        if selected_symbol == '$':
-            if float(fore_money['e_money']) > money_value:
-              fore_money['e_money'] = str(float(fore_money['e_money']) - (money_value * conversion_rate_inr_to_usd))
-              user_currency['money_usd'] = str(float(user_currency['money_usd']) + money_value)
-            else:
-              self.label_18.text = "no balance"
-        elif selected_symbol == '₹':
-            # Transfer e-money to INR (Not required as it's already in e-money)
-            if float(fore_money['e_money']) > money_value:
-                # Update e-money and INR balance
-                fore_money['e_money'] = str(float(fore_money['e_money']) - money_value)
+     acc = self.dropdown_account_numbers.selected_value
+     user_currency = anvil.server.call('get_currency_data', acc)
+     fore_money = anvil.server.call('get_accounts_emoney', acc)
+
+     selected_symbol = self.drop_down_3.selected_value
+     money_value = float(self.text_box_1.text)
+
+     conversion_rate_usd_to_inr = 80.0
+     conversion_rate_swis_to_inr = 95.0
+     conversion_rate_euro_to_inr = 90.0
+
+     if selected_symbol == '$':
+        conversion_rate_inr_to_selected = 1 / conversion_rate_usd_to_inr
+     elif selected_symbol == '₣':
+        conversion_rate_inr_to_selected = 1 / conversion_rate_swis_to_inr
+     elif selected_symbol == 'Є':
+        conversion_rate_inr_to_selected = 1 / conversion_rate_euro_to_inr
+     elif selected_symbol == '₹':
+        conversion_rate_inr_to_selected = 1  # No conversion needed for INR
+     else:
+        self.label_14.text = "Error: Invalid currency symbol selected."
+        return
+
+     if conversion_rate_inr_to_selected:
+        fore_money_value = float(fore_money['e_money'])
+        if fore_money_value >= money_value:
+            fore_money['e_money'] = str(fore_money_value - money_value)
+            if selected_symbol == '₹':
                 user_currency['money_inr'] = str(float(user_currency['money_inr']) + money_value)
-
-                # Update currencies datatable for money_inr
-                app_tables.currencies.add_row(
-                    money_usd=0,
-                    money_inr=money_value,
-                    money_euro=0,  # Adjust these values based on conversions
-                    money_swis=0,
-                    date=current_datetime,
-                    transaction_type="no",
-                    user=self.user['username']
-                )
             else:
-                self.label_18.text = "Insufficient e-money balance"
-
-        # Add conditions for other currency transfers (Euro, Swiss Franc) similarly
-
+                user_currency[selected_symbol.lower()] = str(float(user_currency[selected_symbol.lower()]) + (money_value * conversion_rate_inr_to_selected))
+            new_transaction = app_tables.transactions.add_row(
+                user=self.user['username'],
+                casa=int(acc),
+                e_wallet=fore_money['e_wallet'],
+                money=f"-{selected_symbol}-{money_value}",
+                date=datetime.now(),
+                transaction_type=f"Money transferred from e_money to {selected_symbol}"
+            )
         else:
-            pass  
+            self.label_4.text = "Insufficient e_money balance for the transfer"
 
-      else:
-            pass  # Handle other selected symbols if needed
+    # Update UI or perform any necessary actions after the transfer
+     self.display()
 
+
+
+    
 
     def link_8_click(self, **event_args):
       open_form('deposit',user= self.user)

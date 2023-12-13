e7366c82e652e3f0ec2542736a85ea661a72e9c9
suryaacharya7892@gmail.com <suryaacharya7892@gmail.com>
2023-12-13 15:23:38 +0000

    Edited admin.set_limit

--- client_code/admin/set_limit/__init__.py
+++ client_code/admin/set_limit/__init__.py
@@ -8,6 +8,26 @@ from anvil.tables import app_tables
 class set_limit(set_limitTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+        self.init_components(**properties)
+        self.text_box_8.visible = False
+        self.text_box_10.visible = False
+        self.text_box_9.visible = False
+        self.text_box_11.visible = False
+        self.text_box_12.visible = False
 
-    # Any code you write here will run before the form opens.
+        self.label_9.visible = False
+        self.label_10.visible = False
+        self.label_11.visible = False
+        self.label_12.visible = False
+        self.label_13.visible = False
+      
+        
+
+
+        # Initialize the dropdown with account numbers
+        self.populate_account_dropdown(user_data)
+
+        if user_data:
+            # Any code you write here will run before the form opens.
+            self.populate_textboxes(user_data)
+            self.toggle_edit_mode()

b4bf48a24d9bfb11da0b944c509d36fa69d092a2
thapa07lakshya@gmail.com <thapa07lakshya@gmail.com>
2023-12-13 18:18:50 +0000

    Edited transfer.e_wallet_to_e_wallet and ServerModule1

--- client_code/transfer/e_wallet_to_e_wallet/__init__.py
+++ client_code/transfer/e_wallet_to_e_wallet/__init__.py
@@ -17,13 +17,20 @@ class e_wallet_to_e_wallet(e_wallet_to_e_walletTemplate):
     def button_1_click(self, **event_args):
         depoitor = self.text_box_1.text
         wallet_id = self.text_box_2.text
-        transfer_amount = float(self.text_box_3.text)
-        
-        # selected_symbol = self.drop_down_1.selected_value
+        transfer_amount = self.text_box_3.text
         depositor_wallet_id= anvil.server.call('generate_unique_id', self.user['username'], self.user['phone'])
+        #getting the depositor's details
         fore_money_depositor = anvil.server.call('get_accounts_emoney_using_wallet_id', depositor_wallet_id)
+        #getting the reciever's details
+        fore_money_sent = anvil.server.call('get_accounts_emoney_using_wallet_id',wallet_id)
+        #calculating the money to be added in the recieve's end
+        transfer_fianl_sent_amount= float(fore_money_sent['e_money']) + transfer_amount
+        #calculating the money to be deducted in the depositor's end
         transfer_amount_final = float(fore_money_depositor['e_money'])-transfer_amount
-        anvil.server.call('update_all_rows',wallet_id, float(transfer_amount_final))
+        #setting the value
+        anvil.server.call('update_rows_emoney_trasaction',depositor_wallet_id, str(transfer_amount_final))
+        anvil.server.call('update_rows_emoney_trasaction',wallet_id, str(transfer_fianl_sent_amount))
+      
         
         
 

0b9643a9c02511df17570010a2c16b0f511ca1b6
shivaprasadgowda9380@gmail.com <shivaprasadgowda9380@gmail.com>
2023-12-13 09:56:36 +0000

    Edited admin.transaction_monitoring and admin

--- client_code/admin/transaction_monitoring/__init__.py
+++ client_code/admin/transaction_monitoring/__init__.py
@@ -6,8 +6,24 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 class transaction_monitoring(transaction_monitoringTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+        self.load_transactions()
 
-    # Any code you write here will run before the form opens.
+    def load_transactions(self):
+        """Load transactions into the repeating panel."""
+        self.repeating_panel_1.items = app_tables.transactions.search()
+
+    def add_user_transaction(self, username):
+        """Add user's transactions to the repeating panel."""
+        user_transactions = app_tables.transactions.search(username=username)
+        self.repeating_panel_1.items = user_transactions
+
+    def button_1_click(self, **event_args):
+        """This method is called when the button is clicked."""
+        # Get the username from the textbox
+        username = self.text_box_1.text
+
+        # Add user's transactions to the repeating panel
+        self.add_user_transaction(username)

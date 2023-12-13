89827d7929b4f42375d060879d3c199ed3f83d2e
sasivenkat143143@outlook.com <sasivenkat143143@outlook.com>
2023-12-13 09:37:58 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -11,3 +11,7 @@ class Home(HomeTemplate):
   def link_1_click(self, **event_args):
     open_form('LOGIN')
 
+  def link_10_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+

8445876620dfcf3dad02c8cb4c84f51b2128c8a5
kohilain16@gmail.com <kohilain16@gmail.com>
2023-12-18 16:54:13 +0000

    Edited admin.set_limit

--- client_code/admin/set_limit/__init__.py
+++ client_code/admin/set_limit/__init__.py
@@ -5,6 +5,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from datetime import datetime
 
 class set_limit(set_limitTemplate):
   def __init__(self, user_data=None, **properties):
